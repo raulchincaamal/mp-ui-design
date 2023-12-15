1f05d79a9d457ee94e115cbbd93726b4802d580f
Raul Chin Caamal <raul.chim@macropay.mx>
2023-12-15 13:54:45 -0600

    feat(front): add text wrapper

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -41,7 +41,7 @@ export const Button = ({
       onClick={!isDisabled ? onClick : undefined}
       disabled={isDisabled}
     >
-      {children}
+      <span>{children}</span>
     </button>
   )
 }
