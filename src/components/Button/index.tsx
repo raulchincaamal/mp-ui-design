e431bdf54d1f59bb6951fea475d3d7cf8e411ca7
Raul Chin Caamal <raul.chim@macropay.mx>
2023-12-15 12:21:58 -0600

    build(front): config prettier

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,31 +1,37 @@
-import { IButton, IColorVariants, Type } from './../interfaces'
+import { IButton, IColorVariants, Type } from "./../interfaces"
 
 const colorVariants: Record<Type, IColorVariants> = {
   default: {
-    bgColor: 'bg-blue-800 border-blue-800 hover:bg-blue-900 hover:border-blue-900 shadow ',
-    color: 'text-white',
+    bgColor:
+      "bg-blue-800 border-blue-800 hover:bg-blue-900 hover:border-blue-900 shadow ",
+    color: "text-white",
   },
   primary: {
     bgColor:
-      'bg-yellow-400 border-yellow-400 text-blue-800 hover:bg-yellow-500 hover:border-yellow-500 shadow ',
-    color: 'text-blue-800',
+      "bg-yellow-400 border-yellow-400 text-blue-800 hover:bg-yellow-500 hover:border-yellow-500 shadow ",
+    color: "text-blue-800",
   },
   secondary: {
     bgColor:
-      'bg-white border-blue-800 text-blue-800 hover:text-blue-900 hover:border-blue-900 shadow',
-    color: 'text-blue-900',
+      "bg-white border-blue-800 text-blue-800 hover:text-blue-900 hover:border-blue-900 shadow",
+    color: "text-blue-900",
   },
   text: {
-    bgColor: 'border-zinc-300 border-opacity-0',
-    color: 'text-blue-800 hover:text-blue-900',
+    bgColor: "border-zinc-300 border-opacity-0",
+    color: "text-blue-800 hover:text-blue-900",
   },
   danger: {
-    bgColor: 'bg-red-500 border border-red-500 hover:bg-red-600 shadow',
-    color: 'text-white',
+    bgColor: "bg-red-500 border border-red-500 hover:bg-red-600 shadow",
+    color: "text-white",
   },
 }
 
-export const Button = ({ children, type = 'default', onClick, isDisabled }: IButton) => {
+export const Button = ({
+  children,
+  type = "default",
+  onClick,
+  isDisabled,
+}: IButton) => {
   const { bgColor, color } = colorVariants[type]
   const styleTextColor = `text-center ${color} firs font-normal font-['Avenir LT Pro'] sm:text-sm md:text-sm leading-snug lg:text-base xl:text-base sm:leading-snug md:leading-snug lg:leading-normal xl:leading-normal`
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 import "./input.css"
 
-import App from './App'
+import App from "./App"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
